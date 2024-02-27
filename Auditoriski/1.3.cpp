f8573c5ccf2d38065f6dcf5b8fafd0e451ca94cb
Filip Iliev <151552696+grga1@users.noreply.github.com>
2024-02-28 00:36:32 +0100

    1.3.cpp

---
+++ Auditoriski/1.3.cpp
@@ -0,0 +1,44 @@
+/* Да се напише програма која ќе го пресметува векторскиот и скаларниот производ на два вектори. 
+Векторите се претставени со координати во тродимензионален координатен систем.
+Скаларниот и векторскиот производ да се пресметуваат со посебни функции. За вектор да се дефинира посебна структура vector. 
+*/
+
+#include<iostream>
+
+using namespace std;
+
+
+struct Vector {
+    float x;
+    float y;
+    float z;
+
+    float scalarProduct(Vector other) {
+        return x * other.x + y * other.y + z * other.z;
+    }
+
+    Vector vectorProduct(Vector other) {
+        Vector result;
+        result.x = y * other.z - z * other.y;
+        result.y = z * other.x - x * other.z;
+        result.z = x * other.y - y * other.x;
+        return result;
+    }
+
+    void print() {
+        cout << "[" << x << ", " << y << ", " << z << "]" << endl;
+    }
+};
+
+
+int main() {
+    Vector v1 = {2, 4, 6};
+    Vector v2 = {3, 5, 9};
+
+    cout << "SCALAR PRODUCT" << endl;
+    cout << "v1 * v2 = " << v1.scalarProduct(v2) << endl;
+
+    cout << "VECTOR PRODUCT" << endl;
+    v1.vectorProduct(v2).print();
+    return 0;
+}
